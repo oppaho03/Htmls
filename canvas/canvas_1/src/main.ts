686d133670d205a7f9e394c2dafe97084b886f0d
YoonSung Park <oppaho03@gmail.com>
2022-06-28 05:24:17 +0900

    canvas 1. 100% complated. particles following mouse

--- canvas/canvas_1/src/main.ts
+++ canvas/canvas_1/src/main.ts
@@ -7,6 +7,41 @@ if ( canvas ) {
     // animate();
 }
 
+// focus object
+const mousePos = {
+    x : 0,
+    y : 0
+};
+
+type mousePosObject = {
+    x : number,
+    y : number
+}
+
+// data : colorpallet
+// from Adobe Color cc
+const colorPallet = [ 
+    "#F28442",
+    "#FDD99F",
+    "#C0D1B8",
+    "#98ACB3",
+    "#00838D"
+];
+
+// event bind : resize
+window.addEventListener( 'resize', () => {
+    if ( canvas ) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+} );
+
+// event bind : mousemove
+window.addEventListener( 'mousemove', ( e : MouseEvent ) => {
+    mousePos.x = e.x;
+    mousePos.y = e.y;
+} );
+
 function animate() : void {        
     const cxt = canvas?.getContext('2d');
     if ( !canvas || !cxt ) return;
@@ -21,7 +56,7 @@ function animate() : void {
     requestAnimationFrame(animate); // Recursion Function 
 
     // item.update();
-    circleArray.forEach( item => item.update() );
+    circleArray.forEach( item => item.update( mousePos ) );
 }
 
 class Circle {    
@@ -30,7 +65,11 @@ class Circle {
     y : number;    
     mx : number; //(Math.random() - 0.5) * 8;
     my : number; //(Math.random() - 0.5) * 8;
-    radius : number;
+    radius : number;    
+    minRadius : number = 2;
+    maxRadius = 50;    
+    
+    fillColor : string;
 
     constructor(x:number, y:number, mx : number, my : number, radius : number) {
         
@@ -39,6 +78,10 @@ class Circle {
         this.mx = mx;
         this.my = my;
         this.radius = radius;
+        this.minRadius = radius;
+
+        // this.fillColor = colorPallet[Math.random() * colorPallet.length];
+        this.fillColor = colorPallet[Math.floor(Math.random() * colorPallet.length)]; // 소수점 반올림
 
     }
 
@@ -49,11 +92,14 @@ class Circle {
 
         cxt.beginPath();
         cxt.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        cxt.strokeStyle = 'blue';
-        cxt.stroke();
+        // cxt.strokeStyle = 'blue';
+        // cxt.stroke();
+        
+        cxt.fillStyle = this.fillColor;
+        cxt.fill();
     }
 
-    update () {
+    update ( mousePos : mousePosObject ) {
 
         if ( this.x + this.radius > innerWidth || this.x - this.radius < 0  ) {
             this.mx *= -1;
@@ -65,18 +111,45 @@ class Circle {
         this.x += this.mx;
         this.y += this.my;
 
+        // interactivity
+        if ( mousePos.x - this.x < 50 
+            && mousePos.x - this.x > -50 
+            && mousePos.y - this.y < 50 
+            && mousePos.y - this.y > -50 ) {
+            // circle 인스터스와 mouse 좌표 근접 시
+            if ( this.radius < this.maxRadius ) {
+                // this.radius 최대 40px
+                this.radius += 1;
+            }
+        }
+        else if ( this.radius > this.minRadius ) {
+            // circle 인스터스와 mouse 좌표 멀어질 시
+            // + this.radius 최소 2px 리사이즈
+            this.radius -= 1;
+        }
+        
+      
         this.draw();        
     }
 }
 
+
 const circleArray : Circle[] = [];
-for ( let count = 0; count < 100; count ++ ) {
-    // let [x, y, mx, my] = [ 200, 200 , 4, 4];
-    let [x, y, mx, my] = [ Math.random() * innerWidth, Math.random() * innerHeight, (Math.random() - 0.5) * 4,  (Math.random() - 0.5) * 4 ];
+for ( let count = 0; count < 500; count ++ ) {
+    // let [x, y, mx, my, radius] = [ 200, 200 , 4, 4, 30];
+    // let x = Math.random() * innerWidth;
+    // let y = Math.random() * innerHeight;
+    let radius = Math.random() * 3 + 1;    
+    let x = Math.random() * (innerWidth - radius * 2) + radius;
+    let y = Math.random() * (innerHeight - radius * 2) + radius;
 
-    circleArray.push(new Circle(x, y, mx, my, 30));
+    
+    // mi : move intval
+    // * INT 값에 비례하여 속도 증가
+    let mi = (Math.random() - 0.5) * 4; 
+    let mx = mi, my = mi;
+
+    circleArray.push(new Circle(x, y, mx, my, radius));
 }
-
-console.log(circleArray);
 
 animate();
